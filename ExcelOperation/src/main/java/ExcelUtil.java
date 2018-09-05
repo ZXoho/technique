6daae54dad09be68ba9ae3a8fdc20d6b344c7f33
ZXoho <348105852@qq.com>
2018-09-05 21:06:07 +0800

    数据库导出excel

--- ExcelOperation/src/main/java/ExcelUtil.java
+++ ExcelOperation/src/main/java/ExcelUtil.java
@@ -1,23 +1,28 @@
-package main.java;
-
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import jxl.read.biff.BiffException;
+import org.apache.poi.hssf.usermodel.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by ZXoho
  * 2018/9/3 21:05
  * 导入Excel
- * https://blog.csdn.net/lulin27861/article/details/19070001
  */
 
   public class ExcelUtil {
 
+  /**
+   * 将Excel文件导入数据库
+   * @param request
+   * @throws BiffException
+   * @throws IOException
+   */
     public static void insert(HttpServletRequest request) throws BiffException, IOException {
 
       String path = request.getServletPath(); //???
@@ -63,7 +68,42 @@ import java.io.IOException;
         jdbc.executeUpdate(sql);
       }
 
+      //断开连接
       jdbc.closeStmt();
       jdbc.closeConnection();
+    }
+
+    /**
+     * 数据库表转成Excel
+     * @param list
+     * @return
+     */
+    public static HSSFWorkbook tableToExcel(List<InsertData> list) {
+
+        String[] excelHeader = {"姓名", "年级", "成绩"};//excel列名
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("学生表");//TODO 此处表名需再处理
+        HSSFRow row = sheet.createRow(0);
+        HSSFCellStyle cellStyle = wb.createCellStyle();//表格样式
+        cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+
+        //表头赋值
+        for(int i = 0; i < excelHeader.length; i++) {
+            HSSFCell cell = row.createCell(i);
+            cell.setCellStyle(cellStyle);
+            cell.setCellValue(excelHeader[i]);
+            sheet.autoSizeColumn(i);
+            sheet.setColumnWidth(cell.getColumnIndex(), 100 * 50);
+        }
+
+        //表赋值
+        for(int i = 0; i < list.size(); i++) {
+            InsertData student = list.get(i);
+            row = sheet.createRow(i + 1);
+            row.createCell(1).setCellValue(student.getName());
+            row.createCell(2).setCellValue(student.getGrade());
+            row.createCell(3).setCellValue(student.getScore());
+        }
+        return wb;
     }
 }
